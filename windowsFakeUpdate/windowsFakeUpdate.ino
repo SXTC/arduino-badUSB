63d59789b2b59bfa64e8aa2ab91600e2669637f1
haha@example.com <haha@example.com>
2017-06-29 21:33:52 -0300

    update

--- windowsFakeUpdate/windowsFakeUpdate.ino
+++ windowsFakeUpdate/windowsFakeUpdate.ino
@@ -1,41 +1,61 @@
 #include "Keyboard.h"
 
-#define LED_WAITING 11
-#define LED_EXPLOITING 12
-#define LED_DONE 13
-boolean state = true;
+#define RGB 1 
+bool state = true;
 Keyboard key;
+void waiting();
+void exploiting();
+void done();
 
 void setup(){
     Serial.begin(9600);
     delay(1000);
-    pinMode(11,OUTPUT);
-    pinMode(12,OUTPUT);
-    pinMode(13,OUTPUT);
-    pinMode(5,INPUT);
-    digitalWrite(11,HIGH);
+    pinMode(3,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(8,INPUT);
+    #if defined(RGB)
+      waiting();
+    #endif
 }
 
 void loop(){
   delay(1000);
-  if ((digitalRead(5) == HIGH) && state){
-    digitalWrite(11,LOW);
-    digitalWrite(12,HIGH);
-    key.ALT_F2();
-    key.DELAY(100);
-    key.STRING("gnome-terminal");
-    key.DELAY(100);
-    key.ENTER();
+  if ((digitalRead(8) == HIGH) && state){
+    #if defined(RGB)
+      exploiting();
+    #endif
+    //key.ALT_F2();
     key.DELAY(1000);
-    key.STRING("google-chrome http://fakeupdate.net/win10/index.html");
+    //key.STRING("gnome-terminal");
     key.DELAY(100);
-    key.ENTER();
+    //key.ENTER();
     key.DELAY(1000);
-    key.FULL_WINDOW();
-    digitalWrite(12,LOW);
+    //key.STRING("google-chrome http://fakeupdate.net/win10/index.html");
     key.DELAY(100);
-    digitalWrite(13,HIGH);
+    //key.ENTER();
+    key.DELAY(1000);
+    //key.FULL_WINDOW();
+    key.DELAY(100);
+    #if defined(RGB)
+      done();
+    #endif
     state = false;
   }
 
+}
+void waiting(){
+    analogWrite(5,200);
+    analogWrite(3,0);
+    analogWrite(6,0);
+}
+void exploiting(){
+    analogWrite(5,140);
+    analogWrite(3,30);
+    analogWrite(6,180);
+}
+void done(){
+    analogWrite(5,0);
+    analogWrite(3,0);
+    analogWrite(6,200);
 }
